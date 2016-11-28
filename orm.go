27c7d400a8f37d98c74865ac534dbf1cd82c9d77
rakawestu <rakamogandhi@hotmail.com>
2016-11-28 13:39:11 +0700

    Fix card orm

--- orm.go
+++ orm.go
@@ -27,25 +27,22 @@ func SaveCards(userID string, cards []Card) error {
 	c := MongoDB.C(cardCollectionKey)
 	err := c.Find(bson.M{"userid": userID}).All(&savedCards)
 	if err != nil {
-		return err
+		fmt.Println("Error when getting cards: " + err.Error())
 	}
+
 	if len(savedCards) > 0 {
-		for _, card := range cards {
-			card.UserID = userID
-			err1 := c.Update(bson.M{"userid": userID}, card)
-			if err1 != nil {
-				return err1
-			}
-		}
-	} else {
-		for _, card := range cards {
-			card.UserID = userID
-			err2 := c.Insert(&card)
-			if err2 != nil {
-				return err2
-			}
+		_, err1 := c.RemoveAll(bson.M{"userid": userID})
+		if err1 != nil {
+			return err
 		}
 	}
 
+	for _, card := range cards {
+		card.UserID = userID
+		err2 := c.Insert(&card)
+		if err2 != nil {
+			return err2
+		}
+	}
 	return nil
 }
